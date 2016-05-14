76a456bc3d44e2e1e81285ff1ad723b4495bb5a0
Anders <anrskp@gmail.com>
2016-05-14 03:27:19 +0200

    implementet get recent matches

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
 	
+	// todo html5 api history routing
+
 	// Routing
 	$routeProvider.
 	when('/', {
@@ -31,26 +33,37 @@ app.controller('mainController', function($scope, $http) {
 
 	// Fields
 	var baseUrl = 'https://euw.api.pvp.net',
-		apiKey = '?api_key=10505c29-a6ad-4506-9a94-59e6b010f5d8'
-
+		apiKey = '?api_key=10505c29-a6ad-4506-9a94-59e6b010f5d8';
+		
 	// Get summoner info.
 	$scope.getSummonerInfo = function() {	
 		// Searchfield input.
 		var userInput = document.getElementById("userInput").value,
 			url = baseUrl
-			 	  + '/api/lol/EUW/v1.4/summoner/by-name/'
+			 	  + '/api/lol/euw/v1.4/summoner/by-name/'
 			 	  + userInput // summoner name
 				  + apiKey;
 
 		$http.get(url).then(function (response) {
 			$scope.newSummoner = response.data[userInput];
+
 			console.log($scope.newSummoner);
+			$scope.getChampions();
 		});
 	};
 
-	// Get 10 most played champions.
+	// Get ten recent matches.
 	$scope.getChampions = function() {
-		// not supported yet.
+		var url = baseUrl 
+				  + "/api/lol/euw/v1.3/game/by-summoner/"
+				  + $scope.newSummoner.id
+				  + "/recent" 
+				  + apiKey;
+
+		$http.get(url).then(function(response) {
+			var info = response;
+			console.log(info);
+		})
 	}
 
 });
